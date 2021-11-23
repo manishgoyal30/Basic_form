0821db36e86f0aca430ad562d428875265886c06
Manish <manishgoyal6681@gmail.com>
2021-11-23 19:52:37 +0530

    improved the forms code and added the three inputs for the form i.e Name, LastName, Email. use the single hook, reduced the functions, used the if else conditions to add the form inputs

--- src/App.js
+++ src/App.js
@@ -35,67 +35,271 @@
 
 //Tutorial 38
 
+// import React, { useState } from "react";
+
+// //defining the hooks
+// const App =() =>{
+  
+//   const [name, setName] = useState();
+//   const [fullName,setFullName] = useState();
+//   const [pass, setPassword] = useState(); 
+//   const [fullPass,setFullPass] = useState();
+
+
+// //Defining the target Values 
+
+//   const inputEvent = (event) => {
+//     console.log(event.target.value);
+//     setName(event.target.value);   
+//   };
+
+
+//   const inputPass = (event) =>{
+//     console.log(event.target.value);
+//     setPassword(event.target.value);
+//   }
+
+
+//   //defining the events to store 
+
+//   const onSubmit = (event) =>{
+//     event.preventDefault(); //here preventDefalut stops tne default practice of refreshing the page while submiting
+//     setFullName(name);
+//     setFullPass(pass);
+//   };
+
+
+
+// //Main body
+// return(
+// <>
+// {/*use of onSubmit direct triggers the onsubmit function by the sunmit button we dont have to pass tha  function on the button*/}
+
+// <form onSubmit={onSubmit}>  
+
+// <div>
+//   <h1>Hello {fullName} {fullPass}</h1>
+
+//   <input
+//    type="text" 
+//    placeholder="Enter Your Name" 
+//    onChange={inputEvent}  //always use the function name
+//    value={name}  // use the initial value of the hook
+//    />
+
+// <input
+//    type="text"  
+//    placeholder="Enter the Password" 
+//    onChange={inputPass} //Always use the function name
+//    value={pass}  //use the initial value of hooks
+//    />
+  
+// <br/>
+
+//   <button type="submit">Submit </button>
+//   </div>
+//   </form>
+//   </>
+// );
+
+
+// };
+// export default App;
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+//Tutorial 39  ***********************************
+
+
+//Handling Complex Multiple Input Form States in React JS in Hindi
+//simplification of the forms
+
+
+// import React, { useState } from "react";
+
+// //defining the hooks
+// const App =() =>{
+  
+//   //Replaced to make it simpler
+//   // const [name, setName] = useState();
+//   // const [fullName,setFullName] = useState();
+//   // const [last, setLast] = useState(); 
+//   // const [fullLast,setFullLast] = useState();
+
+// //using only a single Hook 
+//   const [name, setName] = useState();
+
+// //Defining the target Values
+//   const inputEvent = (event) => {
+//     console.log(event.target.value);
+//     setName(event.target.value);   
+//   };
+
+
+//   //defining the events to store 
+
+//   const onSubmit = (event) =>{
+//     event.preventDefault(); //here preventDefalut stops tne default practice of refreshing the page while submiting
+
+//     // setFullName(name);   //****replaced****
+//     // setFullLast(last);    //****replaced****
+//   };
+
+
+
+// //Main body
+// return(
+// <>
+// {/*use of onSubmit direct triggers the onsubmit function by the sunmit button we dont have to pass tha  function on the button*/}
+
+// <form onSubmit={onSubmit}>  
+
+// <div>
+//   <h1>Hello {fullName} {fullLast}</h1>
+//   <h3>{email}</h3>
+
+//   <input
+//    type="text" 
+//    placeholder="Enter Your First Name" 
+//    onChange={inputEvent}  //always use the function name
+//    value={name}  // use the initial value of the hook
+//    />
+
+// <input
+//    type="text"  
+//    placeholder="Enter the Last Name" 
+//   //  onChange={inputLast} //Always use the function name  //****replaced****
+//   onChange = {inputEvent} //we will call only a single function in all the input fields
+//    value={last}  //use the initial value of hooks
+//    />
+
+
+
+// <input
+//    type="email"  
+//    placeholder="Enter the Email"
+   
+//    //****replaced****
+//   //  onChange={inputLast} //Always use the function name
+//   onChange = {inputEvent} //we will call only a single function in all the input fields
+
+//    value={last}  //use the initial value of hooks
+//    />
+  
+// <br/>
+
+//   <button type="submit">Submit </button>
+//   </div>
+//   </form>
+//   </>
+// );
+
+
+// };
+// export default App;
+
+
+
+//*************Updated code*************/
+//*************tutotial 39**************/
+
 import React, { useState } from "react";
 
-//defining the hooks
-const App =() =>{
-  
-  const [name, setName] = useState();
-  const [fullName,setFullName] = useState();
-  const [pass, setPassword] = useState(); 
-  const [fullPass,setFullPass] = useState();
 
-
-//Defining the target Values 
+const App = () =>{
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+  });
 
   const inputEvent = (event) => {
-    console.log(event.target.value);
-    setName(event.target.value);   
-  };
+    console.log(event.target.value);  
+    console.log(event.target.name);
+    
+
+    // will target the tags in the input
+
+    // const value = event.target.value;
+    // const name = event.target.name;
 
 
-  const inputPass = (event) =>{
-    console.log(event.target.value);
-    setPassword(event.target.value);
-  }
+    const {value, name}= event.target;
+    
 
-
-  //defining the events to store 
+    setFullName((preValue) => {
+      // console.log(preValue);
+      if(name === "fName") {
+      return{
+        fname: value,
+        lname: preValue.lname,
+        email: preValue.email,
+      };               
+      } else if(name === "lName") {
+        return{
+          fname: preValue.fname,
+          lname: value,
+          email: preValue.email,
+        }; 
+      } 
+      else if(name === "email") {
+        return{
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email: value,
+        }; 
+      }             
+    });   
+   
+  };    
 
   const onSubmit = (event) =>{
-    event.preventDefault(); //here preventDefalut stops tne default practice of refreshing the page while submiting
-    setFullName(name);
-    setFullPass(pass);
-  };
+    event.preventDefault(); 
+    alert("Form Submitted");
+  };  
 
 
-
-//Main body
 return(
 <>
-{/*use of onSubmit direct triggers the onsubmit function by the sunmit button we dont have to pass tha  function on the button*/}
-
-<form onSubmit={onSubmit}>  
-
+<form onSubmit={onSubmit}> 
 <div>
-  <h1>Hello {fullName} {fullPass}</h1>
+  <h1>Hello {fullName.fname} {fullName.lname} </h1> 
+  <h3>{fullName.email}</h3> 
 
   <input
    type="text" 
-   placeholder="Enter Your Name" 
-   onChange={inputEvent}  //always use the function name
-   value={name}  // use the initial value of the hook
+   placeholder="Enter Your First Name" 
+   name="fName"
+   onChange={inputEvent}  
+   value={fullName.fname} 
    />
+<br/>
 
 <input
    type="text"  
-   placeholder="Enter the Password" 
-   onChange={inputPass} //Always use the function name
-   value={pass}  //use the initial value of hooks
+   placeholder="Enter the Last Name"   
+   name ="lName"
+   onChange = {inputEvent} 
+   value={fullName.lname}  
    />
-  
-<br/>
 
+<input
+   type="email"  
+   placeholder="Enter the Email"   
+   name ="email"
+   onChange = {inputEvent} 
+   value={fullName.email}  
+   />
   <button type="submit">Submit </button>
   </div>
   </form>
@@ -107,10 +311,3 @@ return(
 export default App;
 
 
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-//Tutorial 39
