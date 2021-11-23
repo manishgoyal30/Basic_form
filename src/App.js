a3fd085366369631d7636388aa11782a44ad8daf
Manish <manishgoyal6681@gmail.com>
2021-11-23 17:50:06 +0530

    tutotial 37

---
+++ src/App.js
@@ -0,0 +1,68 @@
+// import React, { useState } from "react";
+
+// const App =() =>{
+
+//   const [name, setName] = useState();  
+//   const[fullName, setFullName] = useState();
+
+//   const inputEvent = (event) => {
+//      setName(event.target.value);    
+//   };
+
+//   const onSubmit =() =>{
+//     setFullName(name);
+//   };
+
+//   return(
+//   <>
+//   <div>
+//     <h1> Hello {fullName}</h1>
+//     <input type = 'text'  placeholder= "Enter Your Name" onChange={inputEvent} value={name}/>
+
+//     <button onClick={onSubmit}> Submit  </button>
+//   </div>
+// </>
+// );
+// };
+
+// export default App;
+
+
+
+import React, { useState } from "react";
+
+const App =() =>{
+  
+  const [name, setName] = useState(); 
+
+  const [fullName,setFullName] = useState();
+
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    setName(event.target.value);
+  };
+
+  const onSubmit = () =>{
+    setFullName(name);
+  };
+return(
+<>
+<div>
+  <h1>Hello {fullName}</h1>
+
+  <input type="text" placeholder="Enter Your Name" onChange={inputEvent} value={name}/>
+  
+
+  <button onClick={onSubmit}>Submit </button>
+  </div>
+  </>
+);
+
+};
+export default App;
+
+
+
+
+
+  
