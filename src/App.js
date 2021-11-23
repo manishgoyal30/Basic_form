106473c265c430c118e46b73071d25c9fde622f3
Manish <manishgoyal6681@gmail.com>
2021-11-23 18:19:50 +0530

    learn to handle the multiple input inputs in a single form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+
+//tutorial 37
+
 // import React, { useState } from "react";
 
 // const App =() =>{
@@ -27,36 +30,78 @@
 
 // export default App;
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+//Tutorial 38
 
 import React, { useState } from "react";
 
+//defining the hooks
 const App =() =>{
   
-  const [name, setName] = useState(); 
-
+  const [name, setName] = useState();
   const [fullName,setFullName] = useState();
+  const [pass, setPassword] = useState(); 
+  const [fullPass,setFullPass] = useState();
+
+
+//Defining the target Values 
 
   const inputEvent = (event) => {
     console.log(event.target.value);
-    setName(event.target.value);
+    setName(event.target.value);   
   };
 
-  const onSubmit = () =>{
+
+  const inputPass = (event) =>{
+    console.log(event.target.value);
+    setPassword(event.target.value);
+  }
+
+
+  //defining the events to store 
+
+  const onSubmit = (event) =>{
+    event.preventDefault(); //here preventDefalut stops tne default practice of refreshing the page while submiting
     setFullName(name);
+    setFullPass(pass);
   };
+
+
+
+//Main body
 return(
 <>
+{/*use of onSubmit direct triggers the onsubmit function by the sunmit button we dont have to pass tha  function on the button*/}
+
+<form onSubmit={onSubmit}>  
+
 <div>
-  <h1>Hello {fullName}</h1>
+  <h1>Hello {fullName} {fullPass}</h1>
 
-  <input type="text" placeholder="Enter Your Name" onChange={inputEvent} value={name}/>
+  <input
+   type="text" 
+   placeholder="Enter Your Name" 
+   onChange={inputEvent}  //always use the function name
+   value={name}  // use the initial value of the hook
+   />
+
+<input
+   type="text"  
+   placeholder="Enter the Password" 
+   onChange={inputPass} //Always use the function name
+   value={pass}  //use the initial value of hooks
+   />
   
+<br/>
 
-  <button onClick={onSubmit}>Submit </button>
+  <button type="submit">Submit </button>
   </div>
+  </form>
   </>
 );
+
 
 };
 export default App;
@@ -65,4 +110,7 @@ export default App;
 
 
 
-  
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Tutorial 39
